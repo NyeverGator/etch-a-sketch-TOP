93f65651b03bbaa1ab0773a66276bfd113e2e4bf
Keane Dalisay <keane.dalisay0118@gmail.com>
2022-07-29 16:01:21 +0800

    Create variables for each existing button

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const colorBtn = document.getElementById('color');
+const eraseBtn = document.getElementById('erase');
+const fillBtn = document.getElementById('fill');
+const clearBtn = document.getElementById('clear');
+const resizeBtn = document.getElementById('resize');
+
+const usualColorBtn = document.getElementById('usual-color');
+const randomColorBtn = document.getElementById('random-color');
+const toggleGridBtn = document.getElementById('toggle-grid');
